00dbd573eee647a8f73dcace8fa7a5dd5181dc97
JayenKerai <32294741+JayenKerai@users.noreply.github.com>
2020-02-03 19:01:52 +0000

    Add files via upload

---
+++ AlphabetChecker/src/main/java/com/sparta/jk/AlphabetChecker.java
@@ -0,0 +1,13 @@
+package com.sparta.jk;
+
+/*
+- Returns true if constructor parameter contains one of each letter.
+- Uses regex to confirm a match for each character is found.
+ */
+public class AlphabetChecker {
+    String regex = "(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z).*";
+
+    public boolean hasAlphabet(String str) {
+        return str.toLowerCase().matches(regex);
+    }
+}
---
+++ AlphabetChecker/src/main/java/com/sparta/jk/Main.java
@@ -0,0 +1,8 @@
+package com.sparta.jk;
+
+public class Main {
+    public static void main(String[] args) {
+        AlphabetChecker alphabetChecker = new AlphabetChecker();
+        System.out.println(alphabetChecker.hasAlphabet("the quick brown fox jumps over the lazy dog"));
+    }
+}
