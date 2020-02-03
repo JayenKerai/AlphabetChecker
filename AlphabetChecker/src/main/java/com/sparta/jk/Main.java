0b2ee482e83f7734d305b24a54133c9d163d7fde
JayenKerai <32294741+JayenKerai@users.noreply.github.com>
2020-02-03 19:05:49 +0000

    Add files via upload

--- AlphabetChecker/src/main/java/com/sparta/jk/Main.java
+++ AlphabetChecker/src/main/java/com/sparta/jk/Main.java
@@ -3,6 +3,6 @@ package com.sparta.jk;
 public class Main {
     public static void main(String[] args) {
         AlphabetChecker alphabetChecker = new AlphabetChecker();
-        System.out.println(alphabetChecker.hasAlphabet("the quick brown fox jumps over the lazy dog"));
+        System.out.println(alphabetChecker.hasAlphabet("The quick brown fox jumps over the lazy dog"));
     }
 }
